decc19083e3c595f0ecb69d56cc864cd4105d74c
DoubleMindy <doublemind21@gmail.com>
2017-05-14 18:49:54 +0300

    Update alberti.py

--- alberti.py
+++ alberti.py
@@ -1,9 +1,11 @@
 __author__ = 'Timokhin Ilya'
 __license__ = 'SCS-152, HSE MIEM 2017'
 
+import sys
 import random
 import math
-
+from PyQt5.QtWidgets import (QWidget, QLabel, QPushButton, QLineEdit,
+    QInputDialog, QApplication, QCheckBox)
 
 def  rotation(a, pos):
     return a[-pos:] + a[:-pos]
@@ -33,10 +35,9 @@ def Alberti_first(alpha, key, text, decode = True):
         return cicle_rotation(text, alpha, key, decode = False)
     else:
         return cicle_rotation(text, alpha, key)
-
 def Alberti_second(alpha, key, text, decode = True):
     if decode == False:
-        key = rotation(key, 32-key.index(text[0]))   
+        key = rotation(key, len(alpha)-key.index(text[0]))   
     else:
         key = rotation(key, 10)
         while(alpha.index(alpha[key.index(alpha[key.index(OT[0])])]) != 0):
@@ -98,57 +99,63 @@ def Alberti_fifth(alpha, key, text, password, decode = True):
             else:
                 cipher.append(alpha[key.index(text[letter])])
     return ''.join(cipher)
-    
-            
-    
+
+class Disk(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        
+        self.cb = QCheckBox('(Decode text)', self)
+        self.cb.move(30, 250)
+
+        lblk = QLabel('Enter key: ', self)
+        lblk.move(30, 10)
+
+        self.k = QLineEdit(self)
+        self.k.setGeometry(30, 30, 400, 22)
+
+        lbltx = QLabel('Enter text (Open Text or Cipher Text): ', self)
+        lbltx.move(30, 70)
+        
+        self.txt = QLineEdit(self)
+        self.txt.setGeometry(30, 90, 400, 42)
+
+        lblres = QLabel('Result is: ', self)
+        lblres.move(30, 150)
+        
+        self.res = QLineEdit(self)
+        self.res.setGeometry(30, 170, 400, 52)
+
+        self.btn = QPushButton('DO', self)
+        self.btn.move(30, 300)
+
+        self.btn.clicked.connect(self.showDialog)
+        
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Alberti cipher')
+        self.show()
+
+
+    def showDialog(self):
+        alf = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+        self.k = self.k.text().upper().split(' ')
+        checker = [i for i in self.k if i in alf]
+        if ((len(alf) == len(self.k)) and (len(set(self.k)) == len(self.k)) and (len(checker) == len(self.k)) and (len(self.txt.text()) > 0)):
+            if self.cb.isChecked() == True:
+                self.res.setText(str(Alberti_first(alf, self.k, self.txt.text().upper(), decode = True)))
+            else:
+                self.res.setText(str(Alberti_first(alf, self.k, self.txt.text().upper(), decode = False)))
+        else:
+            self.res.setText('Invalid input!')    
+        
+
 
 if __name__ == '__main__':
-    
-    alf = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
-    k = input('Enter key (split it with spaces): ').split(' ')
-    checker = [i for i in k if i in alf]
-    
-    if ((len(alf) == len(k)) and (len(set(k)) == len(k)) and (len(checker) == len(k))):
-        mode = input('Input disk mode (1, 2, 3, 4, 5): ')
-        decode = input('Do you decode text? (print \'Д\' if so, and \'Н\' if not): ')
-        if decode == 'Д':
-            if mode == '1':
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_first(alf, k, OT))
-            if mode == '2':
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_second(alf, k, OT))
-            if mode == '3':
-                indicator = input('Enter indicator: ').upper()
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_third(alf, k, OT, indicator))
-            if mode == '4':
-                OT = list(input('Enter text: '))
-                print(Alberti_fourth(alf, k, OT))
-            if mode == '5':
-                pw = input('Enter password: ').upper()
-                OT = list(input('Enter text: ').upper())
-                decode = True
-                print(Alberti_fifth(alf, k, OT, pw, decode)) 
-        if decode == 'Н':
-            if mode == '1':
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_first(alf, k, OT, decode = False))
-            if mode == '2':
-                OT = list(input('Enter text: ').upper())    
-                print(Alberti_second(alf, k, OT, decode = False))
-            if mode == '3':
-                indicator = input('Enter indicator: ').upper()
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_third(alf, k, OT, indicator, decode = False))
-            if mode == '4':
-                p = int(input('Period: '))
-                OT = list(input('Enter text (without spaces): ').upper())
-                decode = False
-                print(Alberti_fourth(alf, k, OT, decode, p))
-            if mode == '5':
-                pw = input('Enter password: ').upper()
-                OT = list(input('Enter text: ').upper())
-                print(Alberti_fifth(alf, k, OT, pw, decode = False))
-    else:
-        print('Try another key! ')
+
+    app = QApplication(sys.argv)
+    ex = Disk()
+    sys.exit(app.exec_())
