b3eced82a9f5f6fdc88d573606b4328d909b0396
DoubleMindy <doublemind21@gmail.com>
2017-05-16 21:49:45 +0300

    Update alberti.py

--- alberti.py
+++ alberti.py
@@ -4,10 +4,21 @@ import sys
 import math
 import random
 from PyQt5.QtWidgets import (QWidget, QLabel, QPushButton, QLineEdit,
-    QInputDialog, QApplication, QCheckBox, QComboBox)
+                             QApplication, QCheckBox, QComboBox, QTextEdit)
+from PyQt5.QtGui import QFont
 
 def  rotation(a, pos):
     return a[-pos:] + a[:-pos]
+
+def get_random_key(alpha):
+    k = []
+    alpha_copy = alpha
+    for letters in range(0, len(alpha_copy)):
+        rand_letter = alpha_copy[random.randint(0, len(alpha_copy)-1)]
+        k.append(rand_letter)
+        alpha_copy.remove(rand_letter)
+    return k    
+        
 
 def rotate_to_A(alpha, key, x):
     while(alpha.index(alpha[key.index(x)]) != 0):
@@ -29,7 +40,8 @@ def cicle_rotation(text, alpha, new_key, decode = True):
     return ''.join(cipher)
 
 def Alberti_first(alpha, key, text, decode):
-    key = rotation(key, abs(alpha.index(text[0]) - key.index(text[0])))
+    while(alpha[key.index(text[0])] != text[0]):
+                key = rotation(key, 1)
     if decode == False:
         return cicle_rotation(text, alpha, key, decode = False)
     else:
@@ -113,12 +125,30 @@ class Disk(QWidget):
         self.combo.addItems(["Mode 1", "Mode 2",
                         "Mode 3", "Mode 4", "Mode 5"])
         self.combo.move(30, 270)
+        self.combo.activated[str].connect(self.onActive)
+
+        self.tip_for_key = QLabel('What is key?', self)
+        self.tip_for_key.move(30, 55)
+        self.tip_for_key.setToolTip("Сюда (через пробел) вводится содержимое внутреннего <b>диска Альберти</b>")
+        self.tip_for_key.setStyleSheet("QLabel {color:blue}")
+
+        self.rand = QPushButton('Generate key', self)
+        self.rand.setGeometry(320, 50, 100, 20)                  
+
+        self.rand.clicked.connect(self.genKey)
         
         self.cb = QCheckBox('Decode text', self)
         self.cb.move(30, 250)
 
         lblk = QLabel('Enter key: ', self)
         lblk.move(30, 10)
+
+        default_info =  "<b> Первая буква открытого текста совмещается с собой на внешнем диске.<br> \
+Далее происходит поворот внутреннего диска на 1 букву по часовой стрелке, <br> ищется соответствие второй буквы \
+на внешнем диске,  заменяется на букву <br> с внутреннего диска и т.д.</b>"
+        self.bigtip = QLabel(default_info, self)
+        self.bigtip.move(10, 520)
+        self.bigtip.setFont(QFont('Calibri', 10))
 
         self.k = QLineEdit(self)
         self.k.setGeometry(30, 30, 400, 22)
@@ -131,13 +161,18 @@ class Disk(QWidget):
 
         lblres = QLabel('Result is: ', self)
         lblres.move(30, 150)
+
+        lblinfo = QLabel('ИНФОРМАЦИЯ О ДАННОМ РЕЖИМЕ: ', self)
+        lblinfo.move(100, 500)
+        lblinfo.setFont(QFont('Times New Roman', 12))
         
         self.res = QLineEdit(self)
         self.res.setGeometry(30, 170, 400, 52)
 
         self.btn = QPushButton('Code it!', self)
         self.btn.setGeometry(30, 295, 250, 42)
-
+        self.btn.setStyleSheet("QPushButton { background-color: red }"
+                      "QPushButton:pressed { background-color: blue }" )
         lbltx = QLabel('Enter indicator (for 3-rd mode): ', self)
         lbltx.move(30, 340)
 
@@ -162,19 +197,46 @@ class Disk(QWidget):
         
         self.btn.clicked.connect(self.showDialog)
         
-        self.setGeometry(300, 300, 500, 500)
-        self.setWindowTitle('Alberti cipher')
+        self.setGeometry(300, 300, 500, 600)
+        self.setWindowTitle('Alberti Cipher')
         self.show()
 
-
-    def showDialog(self):
-        
-        alp_eng =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-        alp = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+    def onActive(self):
+        if self.combo.currentText() == "Mode 1":
+            info = "<b> Первая буква открытого текста совмещается с собой на внешнем диске.<br> \
+Далее происходит поворот внутреннего диска на 1 букву по часовой стрелке, <br> ищется соответствие второй буквы \
+на внешнем диске,  заменяется на  букву <br> с внутреннего диска и т.д.</b>"
+        if self.combo.currentText() == "Mode 2":
+            info = " <b>Первая буква сообщения на внутреннем диске устанавливается напротив <br> буквы А на внешнем. \
+Далее её соответствие ищется на внешнем диске, затем <br> после каждого зашифрования 1 символа ключ \
+поворачивается <br> на 1 символ \n по часовой стрелке и т.д.</b>"
+        if self.combo.currentText() == "Mode 3":
+            info = "<b>Выбирается индикатор, устанавливаемый напротив первой буквы алфавита. <br> После \
+зашифрования каждой буквы внутренний диск поворачивается <br> на 1 символ по часовой стрелке</b>"
+        if self.combo.currentText() == "Mode 4":
+            info = "<b>Выбирается произвольная буква на внутреннем диске, она устанавливается <br> \
+напротив буквы А на внешнем диске. Эта буква записывается как заглавная. <br>Следующие <i>p</i> букв (<i>p</i> - период) \
+шифруются без движения внутреннего диска. <br> Это действие повторяется, каждый раз устанавливается новая случайная буква.</b>"
+        if self.combo.currentText() == "Mode 5":
+            info = "<b>Выбирается пароль, первая буква которого совмещается с буквой А внешнего <br> диска. \
+           Далее производится шифрование первой буквы  открытого <br> текста, затем с буквой А совмещается \
+           вторая буква  пароля и шифруется вторая <br> буква открытого текста и т.д.</b>"
+        self.bigtip.setText(info)
+        self.bigtip.adjustSize()
+            
+    def genKey(self):
         if self.lang.currentText() == "Русский":
-            alf = alp
+            alf = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
         else:
-            alf = alp_eng
+            alf = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']        
+        self.k.setText(' '.join(get_random_key(alf)))
+        
+    def showDialog(self):
+
+        if self.lang.currentText() == "Русский":
+            alf = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+        else:
+            alf = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
             
         j = self.k.text().upper().split(' ')
         checker = [i for i in self.k.text().upper().split(' ') if i in alf]
